7160735e6a8fad6e9639f47b8c4ae58eaf17d963
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-25 09:56:48 -0400

    Update Chart.jsx

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,6 +3,10 @@ import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
+
+const Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+
 useEffect(() => {
     const fetchAPI = async () => {
      setDailyData(await fetchDailyData());
@@ -13,8 +17,6 @@ useEffect(() => {
     fetchAPI();
 }, [dailyData]);
 
-const Chart = () => {
-    const [dailyData, setDailyData] = useState({});
 
 
 
