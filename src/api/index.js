acb8ea1d8a583fb93a59eed12cec98971ffd198a
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-27 09:38:57 -0400

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -6,14 +6,14 @@ export const fetchDailyData = async() => {
     try {
         const { data } = await axios.get(`${url}/daily`);
 
-       const modifiedData = data.map(dailyData) => ({
+       const modifiedData = data.map((dailyData) => ({
            confirmed: dailyData.confirmed.total,
            deaths: dailyData.deaths.total,
            date: dailyData.reportDate,
        }));
        return modifiedData;
     } catch (error) {
-
+        console.log(error);
     }
 }
 
@@ -30,6 +30,14 @@ export const fetchData = async() => {
         return modifiedData;
     }
     catch(error) {
+
+    }
+}
+
+export const countries = async () => {
+    try {
+
+    } catch(error) {
 
     }
 }
