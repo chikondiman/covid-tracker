d9d0c42cd7a0cef8b2dfe5eeebd36c40bede36b5
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-29 16:18:22 -0400

    Update CountryPicker.jsx

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -10,7 +10,7 @@ const CountryPicker = () => {
             setFetchedCountries(await fetchCountries);
 
         }
-        fetchAPI();
+        // fetchAPI();
     }, [setFetchedCountries]);
 
     return (
